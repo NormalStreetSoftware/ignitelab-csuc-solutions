5d3935e23df25f029f91c9cdf3fb4c0332a34aab
Chris Schreiber <chriss@liatrio.com>
2019-09-30 10:28:25 -0700

    add quote feature

---
+++ lab2-app/features/quote.js
@@ -0,0 +1,13 @@
+### [quote]
+## Quote Feature
+const randomQuotes = require('random-quotes');
+
+module.exports = function(controller) {
+
+  controller.hears('tell me a quote', 'message', async(bot, message) => {
+    const quote = randomQuotes.default();
+    await bot.say(`> "${quote.body}" --${quote.author}`);
+  });
+
+}
+### [quote]
